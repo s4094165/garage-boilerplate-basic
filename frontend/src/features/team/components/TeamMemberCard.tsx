d8e4e4e391184265c30a9571af280bc9563f0714
odisfm <102568741+odisfm@users.noreply.github.com>
2026-08-12 14:37:40 +1000

    feat: truncate bios above specified length with "show more" button

--- frontend/src/features/team/components/TeamMemberCard.tsx
+++ frontend/src/features/team/components/TeamMemberCard.tsx
@@ -1,7 +1,21 @@
+"use client"
+
 import { TeamMemberInfo } from '@/features/team/types'
 import Image from 'next/image'
+import { useState } from 'react'
+
+const TRUNCATE_CHAR_LIMIT = 150
 
 export default function TeamMemberCard({ teamMember }: { teamMember: TeamMemberInfo }) {
+  const bioNeedsTruncation = teamMember.bio.length > TRUNCATE_CHAR_LIMIT
+  const [isExpanded, setIsExpanded] = useState(!bioNeedsTruncation)
+  let truncatedBio = ""
+  if (bioNeedsTruncation) {
+    truncatedBio = teamMember.bio.substring(0, TRUNCATE_CHAR_LIMIT)
+  } else {
+    truncatedBio = teamMember.bio
+  }
+
   return (
     <div className={`flex gap-4 py-6 pl-3 pr-6 bg-zinc-900 rounded-xl border-1 border-zinc-800`}>
       <Image
@@ -16,7 +30,24 @@ export default function TeamMemberCard({ teamMember }: { teamMember: TeamMemberI
           <span className={`font-bold text-lg text-zinc-100`}>{teamMember.name}</span>
           <span className={`text-zinc-300`}>{teamMember.role}</span>
         </div>
-        <p className={`pb-3 text-zinc-200/80 text-sm`}>{teamMember.bio}</p>
+          <div className={`pb-3 text-zinc-200/80 text-sm`}>
+            {
+              isExpanded ?
+                <p>
+                  {teamMember.bio}
+                </p>
+                    :
+                    <p>
+                      {truncatedBio}...
+                      <button
+                        onClick={() => setIsExpanded(true)}
+                        className={`text-zinc-400 cursor-pointer block mt-1`}
+                      >
+                        show more
+                      </button>
+                    </p>
+            }
+          </div>
       </div>
     </div>
   )
