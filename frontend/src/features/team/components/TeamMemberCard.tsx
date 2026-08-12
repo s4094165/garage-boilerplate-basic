66608e3c3dbfe067b69e064113184cf556ee4647
odisfm <102568741+odisfm@users.noreply.github.com>
2026-08-12 20:03:04 +1000

    feat: handle missing team member picture

--- frontend/src/features/team/components/TeamMemberCard.tsx
+++ frontend/src/features/team/components/TeamMemberCard.tsx
@@ -2,9 +2,12 @@
 
 import { TeamMemberInfo } from '@/features/team/types'
 import Image from 'next/image'
+import { User } from 'lucide-react'
 import { useState } from 'react'
 
 const TRUNCATE_CHAR_LIMIT = 150
+
+const picClasses = `h-20 w-20 aspect-square rounded-full object-cover`
 
 export default function TeamMemberCard({ teamMember }: { teamMember: TeamMemberInfo }) {
   const bioNeedsTruncation = teamMember.bio.length > TRUNCATE_CHAR_LIMIT
@@ -17,37 +20,40 @@ export default function TeamMemberCard({ teamMember }: { teamMember: TeamMemberI
   }
 
   return (
-    <div className={`flex gap-4 py-6 pl-3 pr-6 bg-zinc-900 rounded-xl border-1 border-zinc-800`}>
-      <Image
-        src={teamMember.pic.src}
-        alt={`Photo of ${teamMember.name}`}
-        width={100}
-        height={100}
-        className={`h-20 w-20 aspect-square rounded-full object-cover`}
-      />
+    <div className={`flex gap-4 rounded-xl border-1 border-zinc-800 bg-zinc-900 py-6 pr-6 pl-3`}>
+      {teamMember.pic ? (
+        <Image
+          src={teamMember.pic.src}
+          alt={`Photo of ${teamMember.name}`}
+          width={100}
+          height={100}
+          className={`${picClasses}`}
+        />
+      ) : (
+        <div className={`${picClasses} flex flex-col items-center justify-center bg-zinc-950`}>
+          <User width={40} height={40} />
+        </div>
+      )}
       <div className={`flex flex-col gap-2`}>
         <div className={`flex flex-col`}>
-          <span className={`font-bold text-lg text-zinc-100`}>{teamMember.name}</span>
+          <span className={`text-lg font-bold text-zinc-100`}>{teamMember.name}</span>
           <span className={`text-zinc-300`}>{teamMember.role}</span>
         </div>
-          <div className={`pb-3 text-zinc-200/80 text-sm`}>
-            {
-              isExpanded ?
-                <p>
-                  {teamMember.bio}
-                </p>
-                    :
-                    <p>
-                      {truncatedBio}...
-                      <button
-                        onClick={() => setIsExpanded(true)}
-                        className={`text-zinc-400 cursor-pointer block mt-1`}
-                      >
-                        show more
-                      </button>
-                    </p>
-            }
-          </div>
+        <div className={`pb-3 text-sm text-zinc-200/80`}>
+          {isExpanded ? (
+            <p>{teamMember.bio}</p>
+          ) : (
+            <p>
+              {truncatedBio}...
+              <button
+                onClick={() => setIsExpanded(true)}
+                className={`mt-1 block cursor-pointer text-zinc-400`}
+              >
+                show more
+              </button>
+            </p>
+          )}
+        </div>
       </div>
     </div>
   )
--- frontend/src/features/team/types.ts
+++ frontend/src/features/team/types.ts
@@ -4,5 +4,5 @@ export type TeamMemberInfo = {
   name: string;
   role: string;
   bio: string;
-  pic: StaticImageData
+  pic: StaticImageData | null
 }
